87c0452c36cfb1b5e966cfe26b1cbfa6976ad387
Deepthi Venkitaramanan <deepthivenkitaramanan@gmail.com>
2016-07-26 19:55:11 +0530

    Issue #8 - add domain, state, creation time and last change time column to the table

--- dump_webcompat_to_db.py
+++ dump_webcompat_to_db.py
@@ -4,13 +4,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
+import datetime
+
+from db import db_session
+from extract_id_title_url import get_webcompat_data
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from extract_id_title_url import get_webcompat_data
-from db import db_session
-import json
 
 DB_PATH = 'sqlite:////tmp/test.db'
 
@@ -24,13 +26,24 @@ class Issue(Base):
     id = Column(String(128), unique=True, primary_key=True)
     summary = Column(String(256))
     url = Column(String(1024))
+    domain = Column(String(1024))
     body = Column(String(2048))
+    state = Column(String(128))
+    creation_time = Column(DateTime)
+    last_change_time = Column(DateTime)
 
-    def __init__(self, id, summary, url, body):
+    def __init__(self, id, summary, url, domain, body, state, creation_time, last_change_time):
         self.id = id
         self.summary = summary
         self.url = url
+        self.domain = domain
         self.body = body
+        self.state = state
+        self.creation_time = datetime.datetime.strptime(creation_time.split('Z')[0],
+                                                             "%Y-%m-%dT%H:%M:%S")
+        self.last_change_time = datetime.datetime.strptime(last_change_time.split('Z')[0],
+                                                             "%Y-%m-%dT%H:%M:%S")
+
 
 
 def main():
@@ -47,7 +60,7 @@ def main():
     # stuff data into database..
     for bug in data['bugs']:
         db_session.add(
-            Issue(bug['id'], bug['summary'], bug['url'], bug['body']))
+            Issue(bug['id'], bug['summary'], bug['url'], extract_domain_name(bug['url']), bug['body'], bug['state'], bug['creation_time'], bug['last_change_time']))
     db_session.commit()
 
 
